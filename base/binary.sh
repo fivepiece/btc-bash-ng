a301dbb625a2c46411836584a42246fa319528fd
fivepiece <fivepiece@users.noreply.github.com>
2017-01-11 19:47:08 +0200

    add string.sh, also move revbytes to string.sh
    add\correct some comments, name revchunks and make it much quicker

--- base/binary.sh
+++ base/binary.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-
-# printf '%s' STR | bin2bytes : " BYTE BYTE..."
-# bin2bytes <<<"STR" : " BYTE BYTE... 0x0A"
-# bin2bytes FILE : " BYTE BYTE..."
+# outputs any input as bytes separated by newlines
+# useful for setting arrays
+# printf '%s' STR | bin2bytes : " byte byte..."
+# bin2bytes <<<"STR" : " byte byte... 0a"
+# bin2bytes FILE : " byte byte..."
 #alias bin2bytes="od -t x1 -An -v -w1"
 
+# outputs any input in hex
 # prtinf '%s' STR | bin2hexstr : "HEX..."
 # bin2hex <<<"STR" : "HEX...0x0A"
 # bin2hex FILE : "HEX..."
-#alias bin2hexstr="hexdump -v -e '\"\" 1/1 \"%02X\" \"\"'"
+#alias bin2hex="hexdump -v -e '\"\" 1/1 \"%02X\" \"\"'"
 
+# print the raw value of hexstr
 # echo HEX | hex2bin : BIN
 # hex2bin HEX : BIN
-hex2bin-2() {
-
+hex2bin-2()
+{
+    # if $1 is empty, read from stdin
+    # TODO find a way to expand '<<<"$1"' somehow to re-use the loop
     if [[ -z "$1" ]]; then
 
         while read -N2 byte; do
@@ -25,28 +30,4 @@ hex2bin-2() {
            printf '%b' "\\x${byte}"
         done <<<"$1"
     fi
-}
-
-revbytes () 
-{ 
-    local -u hexstr
-    local -i chunk
-    if [[ "${1}" == "" ]]; then
-        read hexstr
-        chunk=2
-    else
-        hexstr="${1}"
-        chunk="${2:-2}"
-    fi
-
-    if (( ${#hexstr} % ${chunk} == 1 )); then
-        hexstr="0${hexstr}"
-    fi
-    local -a revstr
-    for ((i=$(( ${#hexstr}-${chunk} )); i>=0; i-- )); do
-        revstr+=( ${hexstr:$(( i*${chunk} )):${chunk}} )
-    done
-    revstr=${revstr[@]//$'\n'/}
-    #revstr=${revstr^^}
-    printf "${revstr// /}"
 }
