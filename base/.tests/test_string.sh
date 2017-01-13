644e585d2168b416dd94f912506458982cf9635b
fivepiece <fivepiece@users.noreply.github.com>
2017-01-13 19:08:56 +0200

    add a test for revchunks()

--- base/.tests/test_string.sh
+++ base/.tests/test_string.sh
@@ -21,3 +21,33 @@ test_padhexstr()
         fi
     done
 }
+
+test_revchunks()
+{
+    local -au hexpatt hexbmp hexrev
+    hexpatt=( 10 101 10101 101011 01 010 01010 010100 )
+    hexbmp=( 100 1100 11100 011100 001 0011 00011 100011 )
+
+    for (( i=0; i<${#hexpatt[@]}; i++ )); do
+        for try in $( input_walk_pattern "${hexpatt[$i]}" "${hexbmp[$i]}" 64 ); do
+            read hexrev < <( revchunks "${try}" | revchunks )
+            if [[ "${hexrev}" != "${try}" ]]; then
+                printf '%s != %s\n%s != %s\n' '${hexrev}' '${try}' \
+                                              "${hexrev}" "${try}"
+                return 1
+            fi
+        done
+    done
+}
+
+test_string()
+{
+    local -a tests=( "test_padhexstr" "test_revchunks" )
+    for testi in ${tests[@]}; do
+        if ! ${testi}; then
+            printf '%s\n' "error: ${testi//test/}"
+            return 1
+        fi
+    done
+}
+test_string || return 1
