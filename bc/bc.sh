4cc762377663f44a2a510251c1ad6e5bfb4d1a67
fivepiece <fivepiece@users.noreply.github.com>
2017-06-23 16:28:10 +0300

    add hmac

--- bc/bc.sh
+++ bc/bc.sh
@@ -7,6 +7,7 @@ BC_ENV_ARGS='-q -l' ${_bc} \
     ./config.bc \
     ./base/conversion.bc ./base/helpers.bc \
     ./logic/bitwise_logic.bc ./logic/shift_rot.bc \
+    ./hash/hmac_conf.bc ./hash/hmac.bc ./hash/.tests/test_hmac.bc \
     ./math/math_mod.bc ./math/extended_euclidean.bc ./math/tonelli_shanks.bc ./math/root_mod.bc \
     ./ec_math/endomorphism.bc ./ec_math/ec_point.bc ./ec_math/ec_math.bc ./ec_math/jacobian.bc \
     ./ecdsa/ecdsa.bc \
