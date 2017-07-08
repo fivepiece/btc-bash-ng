a7e291ba293ced7c0c9251db6054e3a0a84d31eb
fivepiece <fivepiece@users.noreply.github.com>
2017-07-08 08:47:37 +0300

    don't break travis with huge logs

--- test.sh
+++ test.sh
@@ -8,8 +8,9 @@ b=0
 readarray tests < <(find . -type f -name test_*.sh -printf "${btcb_home}/%P\n")
 for testsh in ${tests[@]}; do
     source ${testsh} || (echo "${testsh} failed" 1>&2; return 1)
-done
+done 2>tests.log
 
 b="$?"
 set +x
+tail -200 tests.log
 return "$b"
