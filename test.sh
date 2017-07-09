95195e49e8f1f2e5ddcfc526d972e874914596a7
fivepiece <fivepiece@users.noreply.github.com>
2017-07-09 19:41:59 +0300

    better tests output

--- test.sh
+++ test.sh
@@ -4,20 +4,20 @@ set -x
 shopt -s expand_aliases
 source ./activate.sh
 set_network_versions bitcoin
-
+set +x
 b=0
 readarray tests < <(find . -type f -name test_*.sh -printf "${btcb_home}/%P\n")
 for testsh in ${tests[@]}; do
-    source ${testsh}
+    set -x
+    source ${testsh} 2>tests.log
+    set +x
+    echo $? 1>&2
     if (( $? != 0 )); then
         echo "${testsh} failed" 1>&2
-        set +x
+        tail -200 tests.log
         return 1
     fi
-    # echo $?
-done 2>tests.log
+done
 
 b="$?"
-set +x
-tail -200 tests.log
 return "$b"
