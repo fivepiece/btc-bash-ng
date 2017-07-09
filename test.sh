0b9fd302824b8a6718bb1f1db6a4c2321afbcd93
fivepiece <fivepiece@users.noreply.github.com>
2017-07-09 20:34:22 +0300

    less garbage, more lines

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+echo "" > tests.log
 set -x
 shopt -s expand_aliases
 source ./activate.sh
@@ -9,15 +10,17 @@ b=0
 readarray tests < <(find . -type f -name test_*.sh -printf "${btcb_home}/%P\n")
 for testsh in ${tests[@]}; do
     set -x
-    source ${testsh} 2>tests.log
-    set +x
-    echo $? 1>&2
-    if (( $? != 0 )); then
+    source ${testsh} 2>&1 | grep -v "read -N2 byte\|printf %b '\\\x[0-F][0-F]\|read -N2 hexchunk" >> tests.log
+    # this used to check "$?".  the best solution for this uglyness is something like a 100mb output limit on travis-ci :)
+    if (( ${PIPESTATUS[0]} != 0 )); then
+        set +x
         echo "${testsh} failed" 1>&2
-        tail -200 tests.log
+        tail -1000 tests.log
         return 1
     fi
+    set +x
 done
 
+set +x
 b="$?"
 return "$b"
