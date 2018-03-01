80f5a7ac4681dec8320fd9d5d11eebacbc7a5ae6
fivepiece <fivepiece@users.noreply.github.com>
2018-03-01 20:21:31 +0200

    activate logic.sh

--- activate.sh
+++ activate.sh
@@ -19,7 +19,7 @@ source "${btcb_home}/bc/bc_env.sh"
 
 btcb_env=( \
     base/binary.sh base/string.sh base/number.sh base/int_convert.sh \
-    hash/hexhash.sh hash/hmac.sh hash/merkle_tree.sh \
+    hash/logic.sh hash/hexhash.sh hash/hmac.sh hash/merkle_tree.sh \
     encode/base58check.sh encode/padding.sh encode/pattern.sh \
     ecdsa/rfc6979.sh ecdsa/ecdsa_ifc.sh ecdsa/contract_hash.sh \
     schnorr/schnorr_ifc.sh )
