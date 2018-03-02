d58dfc8ce4abbe093c170971a411699307287f19
fivepiece <fivepiece@users.noreply.github.com>
2018-03-02 09:54:17 +0200

    add witness prefixes
    don't export, just source

--- bitcoin/activate_bitcoin.sh
+++ bitcoin/activate_bitcoin.sh
@@ -14,28 +14,41 @@ set_network_versions() {
     case "$1" in
 
         bitcoin)
-            export privkeyVer="80"
-            export p2pkhVer="00"
-            export p2shVer="05"
-            export xpubVer="0488B21E"
-            export xprvVer="0488ADE4"
-            export clientname="bitcoin"
+            privkeyVer="80"
+            p2pkhVer="00"
+            p2shVer="05"
+            xpubVer="0488B21E"
+            xprvVer="0488ADE4"
+            clientname="mainnet"
+            p2wpkhVer="bc1qw"
+            p2wshVer="bc1qr"
             ;;
 
         testnet|regtest|mastcoin)
-            export privkeyVer="EF"
-            export p2pkhVer="6F"
-            export p2shVer="C4"
-            export xpubVer="043587CF"
-            export xprvVer="04358394"
-            export clientname="$1"
-            ;;
+            privkeyVer="EF"
+            p2pkhVer="6F"
+            p2shVer="C4"
+            xpubVer="043587CF"
+            xprvVer="04358394"
+            clientname="$1"
+            case "$1" in
+                testnet)
+                    p2wpkhVer="tb1qw"
+                    p2wshVer="tb1qr"
+                    ;;
+                regtest)
+                    p2wpkhVer="bcrt1qw"
+                    p2wshVer="bcrt1qr"
+                    ;;
+            esac
 	esac
+    p2wpkhPref="0016"
+    p2wshPref="0020"
 }
 
 for shenv in ${btcb_bit_env[@]}; do
     . "${btcb_bit_home}/${shenv}"
 done
 
-export __debug_btcbash=1
+__debug_btcbash=1
 set_network_versions 'regtest'
