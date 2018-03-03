14e6c0a0a44411641fd76d176b47d8212c968695
fivepiece <fivepiece@users.noreply.github.com>
2018-03-03 13:13:45 +0200

    quiet debug output in rfc6979

--- ecdsa/rfc6979.sh
+++ ecdsa/rfc6979.sh
@@ -15,47 +15,47 @@ rfc6979 ()
     local -u h1 v k t foundk
     # 3.2.a
     read h1 < <( "${hashfunc}" "${msg}" )
-    decho "\n# key : ${key}"
-    decho "# sighash : ${h1}"
+    # decho "\n# key : ${key}"
+    # decho "# sighash : ${h1}"
 
     # 3.2.b
     read v < <( input_mkbitmap '01' "${vlen}" )
-    decho "# 3.2.b, v : ${v}"
+    # decho "# 3.2.b, v : ${v}"
 
     # 3.2.c
     read k < <( input_mkbitmap '00' "${vlen}" )
-    decho "# 3.2.c, k : ${k}"
+    # decho "# 3.2.c, k : ${k}"
 
     # 3.2.d
     read k < <( hmac "${k}" "${v}00${key}${h1}" "${hashfunc}" )
     # 3.2.e
     read v < <( hmac "${k}" "${v}" "${hashfunc}" )
 
-    decho "# 3.2.d, k : ${k}"
-    decho "# 3.2.e, v : ${v}"
+    # decho "# 3.2.d, k : ${k}"
+    # decho "# 3.2.e, v : ${v}"
 
     # 3.2.f
     read k < <( hmac "${k}" "${v}01${key}${h1}" "${hashfunc}" )
     # 3.2.g
     read v < <( hmac "${k}" "${v}" "${hashfunc}" )
 
-    decho "# 3.2.f, k : ${k}"
-    decho "# 3.2.g, v : ${v}"
+    # decho "# 3.2.f, k : ${k}"
+    # decho "# 3.2.g, v : ${v}"
 
     # 3.2.h
     while :; do
-        decho "start 3.2.h"
+        # decho "start 3.2.h"
         # 3.2.h.1
         t=""
 
         # 3.2.h.2
         while (( ${#t} < ${hlen} )); do
-            decho "start 3.2.h.2"
+            # decho "start 3.2.h.2"
             read v < <( hmac "${k}" "${v}" "${hashfunc}" )
             t="${t}${v}"
-            decho "# 3.2.h.2, v : ${v}"
-            decho "# 3.2.h.2, t : ${t}"
-            decho "end   3.2.h.2"
+            # decho "# 3.2.h.2, v : ${v}"
+            # decho "# 3.2.h.2, t : ${t}"
+            # decho "end   3.2.h.2"
         done
 
         # 3.2.h.3
@@ -66,9 +66,9 @@ rfc6979 ()
         fi
 
         read k < <( hmac "${k}" "${v}00" "${hashfunc}" )
-        decho "# 3.2.h.3, k : ${k}"
-        decho "end   3.2.h"
-#       read v < <( hmac "${k}" "${v}" )   # TODO is this needed?
+        # decho "# 3.2.h.3, k : ${k}"
+        # decho "end   3.2.h"
+        # read v < <( hmac "${k}" "${v}" )   # TODO is this needed?
     done
 }
 
